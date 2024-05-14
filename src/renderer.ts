483c1ef78b5e190698601c90938732cec79a8a16
Julien-Clerc <julien.clerc@epitech.eu>
2024-05-14 12:00:21 +0200

    oui

---
+++ src/renderer.ts
@@ -0,0 +1,14 @@
+function performDivision(): void {
+    const numeratorInput = document.getElementById('numerator') as HTMLInputElement;
+    const denominatorInput = document.getElementById('denominator') as HTMLInputElement;
+    const numerator = parseFloat(numeratorInput.value);
+    const denominator = parseFloat(denominatorInput.value);
+    const resultElement = document.getElementById('result')as HTMLParagraphElement;
+
+    if (denominator === 0) {
+    resultElement.innerText = 'Error: Cannot divide byzero';
+    } else {
+    const result = numerator / denominator;
+    resultElement.innerText = `Result: ${result}`;
+    }
+}
